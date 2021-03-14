cf38fac53c203b35b3f73dcd2d9acc5bb04eeac3
tomide <ayotomiiide@gmail.com>
2021-03-14 15:09:22 +0100

    ft(homepage): display all covers and details on the homepage

--- components/covers/Grid.vue
+++ components/covers/Grid.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
-    <covers-cover-item />
+    <covers-cover-item
+      v-for="cover in covers"
+      :id="cover.id"
+      :key="cover.id"
+      :album="cover.album"
+      :artist="cover.artist"
+      :date="cover.date"
+    />
   </div>
 </template>
+
+<script>
+const getCovers = () =>
+  import('~/db/covers.json').then((data) => data.default || data)
+export default {
+  data() {
+    return {
+      covers: [],
+    }
+  },
+  async fetch() {
+    const covers = await getCovers()
+    this.covers = covers
+  },
+}
+</script>
