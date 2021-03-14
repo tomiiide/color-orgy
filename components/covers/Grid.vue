04d1649233a9e1ab5410f08ad3cecdc9a5fc6757
tomide <ayotomiiide@gmail.com>
2021-03-14 14:16:40 +0100

    ft(homepage): Create homepage skeleton

---
+++ components/covers/Grid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+    <covers-cover-item />
+  </div>
+</template>
