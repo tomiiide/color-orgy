1fa9073e4d9eed9a793735f5528b79906b4c9c55
Godson <imiebo.godson@gmail.com>
2022-03-03 00:22:49 +0100

    minor changes

--- src/components/gridLayout/GridLayout.jsx
+++ src/components/gridLayout/GridLayout.jsx
@@ -1,7 +1,14 @@
 import AlbumCard from "../albumCard/AlbumCard"
 
 
+
 const GridLayout = () => {
+
+  fetch('http://localhost:3000/utils/db/covers.json')
+    // .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+
   return (
     <div className='w-screen grid grid-cols-3 lg:grid-cols-6  gap-y-5'>
         <AlbumCard />
