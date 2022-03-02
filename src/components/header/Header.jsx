107b6f6ff28f17304b04aa5ceca3a10ef35a2c46
Sticklo <mafimisebiadebayo@gmail.com>
2022-03-02 01:46:08 +0100

    Style: make header responsive on large devices

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className=" h-[11.25rem] pt-[68px] md:flex ">
-      <div className="flex flex-col  justify-start pl-[11px]">
-        <h1 className="text-[24px] ">
+    <div className=" h-[11.25rem] pt-[68px] md:flex justify-between md:items-center md:pb-14">
+      <div className="flex flex-col  justify-start pl-[11px] md:h-[136px] md:w-[540px]">
+        <h1 className="text-[24px] md:text-[48px]">
           A Curation of <span className="text-black underline">2020</span>
           <select></select>
         </h1>
-        <h1 className="text-[24px]">Nigerian Albums.</h1>
+        <h1 className="text-[24px] md:text-[48px]">Nigerian Albums.</h1>
       </div>
       <div className="flex justify-end pr-[17px] pb-[23px]">
-        <div className="flex flex-col">
-          <p className="text-[12px]">by</p>
-          <p className="text-[12px] underline">WeTalkSound</p>
+        <div className="flex flex-col md:h-[116px] md:w-[311px]">
+          <p className="text-[12px] md:text-[48px]">by</p>
+          <p className="text-[12px] md:text-[48px] underline">WeTalkSound</p>
         </div>
       </div>
     </div>
