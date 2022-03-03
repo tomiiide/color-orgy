f45c1c6c3edeadad339ba41962cc66475d3c179d
Godson <imiebo.godson@gmail.com>
2022-03-03 15:27:10 +0100

    feat: dynamically loading cover data

--- src/components/albumCard/AlbumCard.jsx
+++ src/components/albumCard/AlbumCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const AlbumCard = () => {
+const AlbumCard = ({ cover }) => {
   return (
     <div className='min-w-[120px] min-h-[120px] pb-[5px]'>
         <div className='w-full h-auto bg-black'><img className='w-full h-auto object-cover' src={process.env.PUBLIC_URL + '/assets/images/album-cover-1.svg'} alt='' /></div>
         <div className='w-full h-auto p-[5px]'>
-            <p className='text-sm font-bold'>LOFN</p>
-            <p className='text-sm text-[#0000007D]'>WeTalkSound</p>
-            <p className='text-xs text-[#0000007D]'>Dec 1</p>
+            <p className='text-sm font-bold'>{cover.album}</p>
+            <p className='text-sm text-[#0000007D]'>{cover.artist}</p>
+            <p className='text-xs text-[#0000007D]'>{cover.date}</p>
         </div>
     </div>
   )
