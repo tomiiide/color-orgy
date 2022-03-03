a7ad7350799892757cd46fa156ffa0b1a2b2bd10
Godson <imiebo.godson@gmail.com>
2022-03-03 23:06:51 +0100

    minor change

--- src/components/albumCard/AlbumCard.jsx
+++ src/components/albumCard/AlbumCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AlbumCard = ({ cover }) => {
   return (
-    <div className='min-w-[120px] min-h-[120px] pb-[5px]'>
+    <div className='min-w-[120px] min-h-[120px] pb-[5px] cursor-pointer'>
         <div className='w-full h-auto bg-black'><img className='w-full h-auto object-cover' src={process.env.PUBLIC_URL + '/assets/images/album-cover-1.svg'} alt='' /></div>
         <div className='w-full h-auto p-[5px]'>
             <p className='text-sm font-bold'>{cover.album}</p>
