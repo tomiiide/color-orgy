058b5db79e3a48b2acc1da270cf1a624b3c2c7f1
Godson <imiebo.godson@gmail.com>
2022-03-24 12:13:17 +0100

    added images for pop up and moved fetch function to homepage, so that data is feteched on rendering app

--- src/components/gridLayout/GridLayout.jsx
+++ src/components/gridLayout/GridLayout.jsx
@@ -1,54 +1,16 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import AlbumCard from "../albumCard/AlbumCard";
 
 
-const GridLayout = ({ isDarkMode }) => {
-
-
-  const ENVIRONMENT = process.env.NODE_ENV;
-
-  // const WTF = process.env.REACT_APP_VERCEL_URL;
-
-  console.log(ENVIRONMENT)
-
-  const fetchUrl = (!(ENVIRONMENT === 'development') ? 'https://color-orgy.vercel.app/db.json' : 'http://localhost:3000/db.json'); 
-
-  console.log(fetchUrl)
-
-  const [covers, setCovers] = useState([])
-
-  useEffect(() => {
-    const getCoverData = async() => {
-      const coversData = await fetchCoverDetails()
-      setCovers(coversData)
-    }
-    getCoverData()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
-
-  const fetchCoverDetails = async() => {
-    const fetchData = await fetch(fetchUrl, {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-      }
-    )
-
-    const retrievedData = await fetchData.json()
-
-    return retrievedData
-  }
-
+const GridLayout = ({ isDarkMode, albums }) => {
   
   return (
     <div className={`w-screen grid grid-cols-3 lg:grid-cols-6  gap-y-5 ${
       isDarkMode ? "dark:bg-black dark:text-white" : "bg-white text-black"
     }`}>
       {
-        covers.map((cover, index) => {
-          return <AlbumCard key={index} cover={cover} position={index} isDarkMode={isDarkMode}/>
+        albums.map((album, index) => {
+          return <AlbumCard key={index} albumDetails={album} position={index} isDarkMode={isDarkMode}/>
         })
       }
     </div>
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -6,13 +6,51 @@ import Loadingpage from "../../components/loadingpage/Loadingpage";
 const Homepage = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [albums, setAlbums] = useState([]);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 10000);
-  // }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 15000);
+  }, []);
+
+
+  const ENVIRONMENT = process.env.NODE_ENV;
+
+  console.log(ENVIRONMENT)
+
+  const fetchUrl = (!(ENVIRONMENT === 'development') ? 'https://color-orgy.vercel.app/db.json' : 'http://localhost:3000/db.json'); 
+
+  console.log(fetchUrl)
+
+  useEffect(() => {
+    const getAlbumsData = async() => {
+      const albumsData = await fetchAlbumsDetails()
+      setAlbums(albumsData)
+      // console.log(albums.length())
+      console.log(albums)
+    }
+    getAlbumsData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  
+
+  const fetchAlbumsDetails = async() => {
+    const fetchData = await fetch(fetchUrl, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+      }
+    )
+
+    const retrievedData = await fetchData.json()
+
+    console.log("Retrived Data", retrievedData)
+
+    return retrievedData
+  }
   
   return (
     <div
@@ -26,7 +64,7 @@ const Homepage = () => {
         <>
           <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
           <div className="w-screen">
-            <GridLayout isDarkMode={isDarkMode} />
+            <GridLayout isDarkMode={isDarkMode} albums={albums}/>
           </div>
         </>
       )}
