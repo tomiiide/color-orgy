cf4feb22c9eb3883e165ae1c76bc5d5b09701caf
Godson <imiebo.godson@gmail.com>
2022-04-22 22:59:57 +0100

    feat: album select feature

--- src/components/albumCard/AlbumCard.jsx
+++ src/components/albumCard/AlbumCard.jsx
@@ -34,10 +34,19 @@ const AlbumCard = ({ albumDetails, position, isDarkMode }) => {
     return (
       <div>
         <div id='albumCardBackground' className='modalBackground' onClick={closeHandleClick}>
-            <div id='albumCard' style={{backgroundImage: 'url(album-cover-1.svg)'}} className='w-[350px] h-fit py-[20px] px-[10px] flex flex-col justify-center items-center z-10 bg-white text-black'>
-              <div className='w-4/5 h-auto bg-black'>
-                <img className='w-full h-auto object-cover' src={process.env.PUBLIC_URL + `/assets/images/${(position % 2) ? 'album-cover-1.svg' : 'album-cover-2.svg'}`} alt='' />
-              </div>
+            <div id='albumCard' className='w-[350px] h-[600px] relative flex flex-col justify-center items-center  bg-white text-black'>
+            <div className='w-full h-full' style={{backgroundImage: `url(/assets/images/${(position % 2) ? 'album-cover-1.svg' : 'album-cover-2.svg'})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: '220%', filter: 'blur(3px)', WebkitFilter: 'blur(3px)'}}></div>
+            <div className=' absolute top-0 bottom-0 left-0 right-0 flex flex-col flex-start items-center py-[40px] px-[10px]'>
+                <div className='w-4/5 h-auto pb-[15px] px-[15px]' style={{backgroundColor: 'rgba(255, 255, 255, 0.424)'}}>
+                  <div className='h-fit py-[4px] flex justify-between items-center'>
+                    <p className='font-bold text-base truncate albumText text-[#725994]'>{albumDetails.album}</p>
+                    <p className='font-bold text-base truncate albumText text-[#725994]'>{albumDetails.artist}</p>
+                  </div>
+                  <div className='w-full h-auto bg-black'>
+                    <img className='w-full h-auto object-cover' src={process.env.PUBLIC_URL + `/assets/images/${(position % 2) ? 'album-cover-1.svg' : 'album-cover-2.svg'}`} alt='' />
+                  </div>
+                </div>
+                
                 <div className='p-[10px]'>
                   <ul>
                     <li className='btns  bg-[#F2E5EF] px-[15px] py-[8px] my-[15px] cursor-pointer rounded-full flex justify-start items-center'><img className='w-[20px] h-[20px] object-cover mr-[10px]' src={process.env.PUBLIC_URL + `/assets/images/Spotify_icon.png`} alt='' /><p className='text-xs'>Listen on Spotify</p></li>
@@ -45,6 +54,7 @@ const AlbumCard = ({ albumDetails, position, isDarkMode }) => {
                     <li className='btns  bg-[#F2E5EF] px-[15px] py-[8px] my-[15px] cursor-pointer rounded-full flex justify-start items-center'><img className='w-[20px] h-[20px] object-cover mr-[10px]' src={process.env.PUBLIC_URL + `/assets/images/audiomack_icon.png`} alt='' /><p className='text-xs'>Listen on Audiomack</p></li>
                   </ul>
                 </div>
+            </div>
             </div>
         </div>
       </div>
